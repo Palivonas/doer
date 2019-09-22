be8850e90197e44208cc0e73d2ed6c5ba6c41240
Andrius Palivonas <andriusp@wix.com>
2019-09-22 14:55:19 +0300

    Test user population in find

--- server/test/services/memberships.test.ts
+++ server/test/services/memberships.test.ts
@@ -1,7 +1,7 @@
 import app from '../../src/app';
 import { Membership } from '../../src/shared/entities';
 import { expect } from 'chai';
-import { givenUser, randomId } from '../utils';
+import { givenMembership, givenUser, randomId } from '../utils';
 import { Paginated } from '@feathersjs/feathers';
 
 const service = app.service('memberships');
@@ -27,9 +27,19 @@ describe('memberships service', () => {
     expect(membership.activityId.toString()).to.equal(payload.activityId);
   });
 
-  it('should populate user', async () => {
+  it('should populate user in get', async () => {
     const { user, params } = await givenUser();
+    const activityId = randomId();
+    const { _id } = await givenMembership(activityId, user._id);
 
+    const membership = await service.get(_id) as Membership;
+
+    expect(membership.user!._id.toString()).to.equal(user._id);
+    expect(membership.user!.name).to.equal(user.name);
+  });
+
+  it('should populate user in find', async () => {
+    const { user, params } = await givenUser();
     const activityId = randomId();
 
     const { _id } = await service.create({ activityId }, params) as Membership;
